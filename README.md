576ddb55c09954b8b67527364951b02c6f810f68
David Mason <david.mason@onthebeach.co.uk>
2015-06-28 12:32:24 +0100

    Add README and install script

---
+++ README.md
@@ -0,0 +1,31 @@
+## Installation
+
+### Prerequisites
+
+Debian/Ubuntu systems:
+
+```
+sudo apt-get install git zsh source-highlight
+```
+
+### Clone git repo
+
+```
+git clone https://github.com/djm256/dotfiles.git
+```
+
+### Setup
+
+```
+dotfiles/install.sh
+```
+
+### Switch to zsh
+
+```
+chsh --shell /bin/zsh
+```
+
+### Finishing off
+
+Log out and back in again, and the installation will be complete.
---
+++ install.sh
@@ -0,0 +1,90 @@
+#/bin/bash
+
+REQUIRED_BINARIES=(
+  git
+  dirname
+  realpath
+  readlink
+)
+
+DOTFILES=(
+  antigen
+  .config/terminator
+  .git_commit_message
+  .gitconfig
+  .gitignore
+  .gitmodules
+  .irbrc
+  .perlcriticrc
+  .rubocop.yml
+  .vim
+  .vimrc
+  .zlogin
+  .zprofile
+  .zshenv
+  .zshrc
+)
+
+DOTFILE_DIR=`dirname $0`
+
+function main {
+  sanity_check
+  update_submodules
+  create_symlinks
+  echo "Done"
+}
+
+function sanity_check {
+  for REQUIRED_BINARY in ${REQUIRED_BINARIES[*]}; do
+    if ! which $REQUIRED_BINARY > /dev/null; then
+      echo "Unable to find required $REQUIRED_BINARY command"
+      exit 1
+    fi
+  done
+}
+
+function update_submodules {
+  echo "Installing git submodules..."
+  git -C $DOTFILE_DIR submodule update --init --quiet
+}
+
+function create_symlinks() {
+  echo "Installing symlinks..."
+
+  for DOTFILE in ${DOTFILES[*]}; do
+    TARGET=$HOME/$DOTFILE
+    SOURCE=`realpath $DOTFILE_DIR/$DOTFILE`
+
+    if [ ! -e "$SOURCE" ]; then
+      echo "Unable to find source dotfile $SOURCE"
+      exit 1
+    fi
+
+    if [ -h $TARGET ]; then
+      if [ "`readlink $TARGET`" = "$SOURCE" ]; then
+        #echo "Correct symlink has already been created for $TARGET"
+        continue
+      fi
+    fi
+
+    if [ -e $TARGET -o -h $TARGET ]; then
+      echo -e "\n$TARGET exists, but isn't the expected symlink to $SOURCE."
+      echo "Let's see if you want to remove it"
+
+      rm -i -r $TARGET
+      if [ -e $TARGET ]; then
+        echo "$TARGET still exists. Failing."
+        exit 1
+      fi
+    fi
+
+    ln -s $SOURCE $TARGET
+    if [ $? = 1 ]; then
+      echo "Problem creating symlink"
+      exit 1
+    fi
+  done
+}
+
+main
+exit 0
